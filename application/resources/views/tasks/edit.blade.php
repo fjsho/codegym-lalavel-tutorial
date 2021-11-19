05a34b8a3a3adc9b6ef3a7b4fad53bcab1b8b9e6
fjsho <gorimura7@gmail.com>
2021-11-19 21:39:51 +0900

    [fix]コメント削除関連のメッセージの修正

--- application/resources/views/tasks/edit.blade.php
+++ application/resources/views/tasks/edit.blade.php
@@ -222,7 +222,7 @@
             
                                 <div class="modal-content py-4 text-left px-6">
                                     <div class="flex justify-between items-center pb-3">
-                                        <p class="text-2xl font-bold">{{ __('Are you sure you want to delete this task?') }}</p>
+                                        <p class="text-2xl font-bold">{{ __('Are you sure you want to delete this comment?') }}</p>
                                         <div class="modal-close cursor-pointer z-50">
                                             <svg class="fill-current text-black" xmlns="http://www.w3.org/2000/svg" width="18" height="18" viewBox="0 0 18 18">
                                                 <path d="M14.53 4.53l-1.06-1.06L9 7.94 4.53 3.47 3.47 4.53 7.94 9l-4.47 4.47 1.06 1.06L9 10.06l4.47 4.47 1.06-1.06L10.06 9z"></path>
@@ -230,7 +230,7 @@
                                         </div>
                                     </div>
             
-                                    <p>{{ __('Are you sure you want to delete this task? Once a task is deleted, all of its resources and data will be permanently deleted.') }}</p>
+                                    <p>{{ __('Are you sure you want to delete this comment? Once a comment is deleted, all of its resources and data will be permanently deleted.') }}</p>
             
                                     <div class="flex justify-end pt-2">
                                         <x-link-button class="modal-close m-2" href="#">
