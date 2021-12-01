e29fbec2921d31e83143b377a7a30bce390ca7a0
fjsho <gorimura7@gmail.com>
2021-12-01 21:46:54 +0900

    [add]画像保存用の各種クラスの作成

---
+++ application/app/Http/Controllers/TaskPictureController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\TaskPicture;
+use Illuminate\Http\Request;
+
+class TaskPictureController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\TaskPicture  $taskPicture
+     * @return \Illuminate\Http\Response
+     */
+    public function show(TaskPicture $taskPicture)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\TaskPicture  $taskPicture
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(TaskPicture $taskPicture)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\TaskPicture  $taskPicture
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, TaskPicture $taskPicture)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\TaskPicture  $taskPicture
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(TaskPicture $taskPicture)
+    {
+        //
+    }
+}
