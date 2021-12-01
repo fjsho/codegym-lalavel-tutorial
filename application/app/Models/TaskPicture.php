211c025446b62a385897c29b64ca440ae8022206
fjsho <gorimura7@gmail.com>
2021-12-01 22:23:28 +0900

    [add]モデルのfillableプロパティ及びリレーションの定義

--- application/app/Models/TaskPicture.php
+++ application/app/Models/TaskPicture.php
@@ -4,8 +4,45 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class TaskPicture extends Model
 {
     use HasFactory;
+    use SoftDeletes;
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'task_id',
+        'file_path',
+        'created_user_id',
+    ];
+
+    /**
+     * ユーザーのフルネーム取得.
+     */
+    public function getKeyAttribute()
+    {
+        return "{$this->task->key}-{$this->id}";
+    }
+
+    /**
+     * 画像を所有しているタスクを取得.
+     */
+    public function task()
+    {
+        return $this->belongsTo(Task::class, 'task_id');
+    }
+
+    /**
+     * 画像を所有しているユーザーを取得.
+     */
+    public function user()
+    {
+        return $this->belongsTo(User::class, 'created_user_id');
+    }
 }
