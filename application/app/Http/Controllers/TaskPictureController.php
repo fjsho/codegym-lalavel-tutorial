43504014d4b02551d6f63756f2c43fbb08b9ae35
fjsho <gorimura7@gmail.com>
2021-12-02 18:11:49 +0900

    [add]storeメソッドの記述

--- application/app/Http/Controllers/TaskPictureController.php
+++ application/app/Http/Controllers/TaskPictureController.php
@@ -2,8 +2,11 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Task;
+use App\Models\Project;
 use App\Models\TaskPicture;
 use Illuminate\Http\Request;
+use App\Http\Requests\TaskPictureStoreRequest;
 
 class TaskPictureController extends Controller
 {
@@ -30,12 +33,25 @@ class TaskPictureController extends Controller
     /**
      * Store a newly created resource in storage.
      *
-     * @param  \Illuminate\Http\Request  $request
+     * @param  \app\Http\Requests\TaskPictureStoreRequest  $request
      * @return \Illuminate\Http\Response
      */
-    public function store(Request $request)
+    public function store(TaskPictureStoreRequest $request, Project $project, Task $task)
     {
-        //
+        $validated = $request->validated();
+
+        if (TaskPicture::create([
+            'task_id' => $task->id,
+            'file_path' => $validated['file_path'],
+            'created_user_id' => $request->user()->id,
+        ])) {
+            $flash = ['success' => __('Picture uploaded successfully.')];
+        } else {
+            $flash = ['error' => __('Failed to upload the picture.')];
+        }
+
+        return redirect()->route('tasks.edit', ['project' => $project->id, 'task' => $task->id])
+            ->with($flash);
     }
 
     /**
