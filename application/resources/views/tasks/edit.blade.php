66906f894838280c696b2a73d590b9724c4ddfc8
fjsho <gorimura7@gmail.com>
2021-12-04 19:34:13 +0900

    [wip]ドラッグ＆ドロップに対応したファイルアップロード処理の仮実装

--- application/resources/views/tasks/edit.blade.php
+++ application/resources/views/tasks/edit.blade.php
@@ -1,5 +1,6 @@
 @section('script')
 <script>
+// モーダルウィンドウに係る処理
     function toggleModal(event) {
         const body = document.querySelector('body');
         const modal = document.querySelectorAll('.modal');
@@ -73,6 +74,59 @@
             toggleModal(evt);
         }
     };
+
+
+//ファイルアップロードに関する処理の記述
+    // ドラッグ&ドロップエリアの取得
+    let fileArea = document.getElementById('dropArea');
+
+    // input[type=file]の取得
+    let fileInput = document.getElementById('file');
+
+//ここから下のドラッグオーバー・ドラッグアウト・ドロップ時の処理はtoggleでまとめられそう
+    // ドラッグオーバー時の処理
+    fileArea.addEventListener('dragover', function(e){
+        e.preventDefault();
+        fileArea.classList.add('dragover');
+    });
+
+    // ドラッグアウト時の処理
+    fileArea.addEventListener('dragleave', function(e){
+        e.preventDefault();
+        fileArea.classList.remove('dragover');
+    });
+
+    // ドロップ時の処理
+    fileArea.addEventListener('drop', function(e){
+        e.preventDefault();
+        fileArea.classList.remove('dragover');
+
+        // ドロップしたファイルの取得
+        const files = e.dataTransfer.files;
+
+        // 取得したファイルをinput[type=file]へ
+        fileInput.files = files;
+        
+        if(typeof files[0] !== 'undefined') {
+            // ファイルが正常に受け取れた際の処理
+            document.forms[uploadform].submit();
+        } else {
+            // ファイルが受け取れなかった際の処理
+        }
+    });
+
+    // input[type=file]に変更があれば実行
+    // もちろんドロップ以外でも発火します
+    fileInput.addEventListener('change', function(e){
+        var file = e.target.files[0];
+        
+        if(typeof e.target.files[0] !== 'undefined') {
+            // ファイルが正常に受け取れた際の処理
+            document.forms['uploadform'].submit();
+        } else {
+            // ファイルが受け取れなかった際の処理
+        }
+    }, false);
 
 </script>
 @endsection
