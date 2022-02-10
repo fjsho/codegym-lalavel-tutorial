3822e1cd096a2d9dbef51294c12238c9bce5ca47
fjsho <gorimura7@gmail.com>
2022-02-10 15:50:29 +0900

    [add]「課題の編集」画面における画像投稿機能テスト作成

---
+++ application/tests/Feature/PictureStoreTest.php
@@ -0,0 +1,300 @@
+<?php
+
+namespace Tests\Feature;
+
+use App\Models\User;
+use App\Models\Project;
+use App\Models\Task;
+use Illuminate\Http\UploadedFile;
+use Illuminate\Support\Facades\Storage;
+use Tests\TestCase;
+
+class PictureStoreTest extends TestCase
+{
+    /**
+     * @test
+     */
+    public function jpg画像を投稿できる()
+    {
+        $user = User::factory()->create();
+        $project = Project::factory()->create([
+            'created_user_id' => $user->id,
+        ]);
+        $task = Task::factory()->create([
+            'created_user_id' => $user->id, 
+        ]);
+
+        $file = UploadedFile::fake()->image('test.jpg');
+        $referer = route('tasks.edit', ['project' => $project->id, 'task' => $task->id]);
+
+        $response = $this->actingAs($user)
+        ->from($referer)
+        ->post(route('task_pictures.store', ['project' => $project->id, 'task' => $task->id]),[
+            'file' => $file,
+        ]);
+
+        Storage::disk('public')->assertExists($file->hashName());
+        $response->assertRedirect($referer);
+    }
+
+    /**
+     * @test
+     */
+    public function jpeg画像を投稿できる()
+    {
+        $user = User::factory()->create();
+        $project = Project::factory()->create([
+            'created_user_id' => $user->id,
+        ]);
+        $task = Task::factory()->create([
+            'created_user_id' => $user->id, 
+        ]);
+
+        $file = UploadedFile::fake()->image('test.jpeg');
+        $referer = route('tasks.edit', ['project' => $project->id, 'task' => $task->id]);
+
+        $response = $this->actingAs($user)
+        ->from($referer)
+        ->post(route('task_pictures.store', ['project' => $project->id, 'task' => $task->id]),[
+            'file' => $file,
+        ]);
+
+        Storage::disk('public')->assertExists($file->hashName());
+        $response->assertRedirect($referer);
+    }
+
+    /**
+     * @test
+     */
+    public function png画像を投稿できる()
+    {
+        $user = User::factory()->create();
+        $project = Project::factory()->create([
+            'created_user_id' => $user->id,
+        ]);
+        $task = Task::factory()->create([
+            'created_user_id' => $user->id, 
+        ]);
+
+        $file = UploadedFile::fake()->image('test.png');
+        $referer = route('tasks.edit', ['project' => $project->id, 'task' => $task->id]);
+
+        $response = $this->actingAs($user)
+        ->from($referer)
+        ->post(route('task_pictures.store', ['project' => $project->id, 'task' => $task->id]),[
+            'file' => $file,
+        ]);
+
+        Storage::disk('public')->assertExists($file->hashName());
+        $response->assertRedirect($referer);
+    }
+
+    /**
+     * @test
+     */
+    public function gif画像を投稿できる()
+    {
+        $user = User::factory()->create();
+        $project = Project::factory()->create([
+            'created_user_id' => $user->id,
+        ]);
+        $task = Task::factory()->create([
+            'created_user_id' => $user->id, 
+        ]);
+
+        $file = UploadedFile::fake()->image('test.gif');
+        $referer = route('tasks.edit', ['project' => $project->id, 'task' => $task->id]);
+
+        $response = $this->actingAs($user)
+        ->from($referer)
+        ->post(route('task_pictures.store', ['project' => $project->id, 'task' => $task->id]),[
+            'file' => $file,
+        ]);
+
+        Storage::disk('public')->assertExists($file->hashName());
+        $response->assertRedirect($referer);
+    }
+
+    /**
+     * @test
+     */
+    public function pdfファイルは投稿できない()
+    {
+        $user = User::factory()->create();
+        $project = Project::factory()->create([
+            'created_user_id' => $user->id,
+        ]);
+        $task = Task::factory()->create([
+            'created_user_id' => $user->id, 
+        ]);
+
+        $file = UploadedFile::fake()->create('test.pdf',500,'application/pdf');
+        $referer = route('tasks.edit', ['project' => $project->id, 'task' => $task->id]);
+
+        $response = $this->actingAs($user)
+        ->from($referer)
+        ->post(route('task_pictures.store', ['project' => $project->id, 'task' => $task->id]),[
+            'file' => $file,
+        ]);
+
+        Storage::disk('public')->assertMissing($file->hashName());
+        $response->assertInvalid();
+        $response->assertRedirect($referer);
+    }
+
+    /**
+     * @test
+     */
+    public function txtファイルは投稿できない()
+    {
+        $user = User::factory()->create();
+        $project = Project::factory()->create([
+            'created_user_id' => $user->id,
+        ]);
+        $task = Task::factory()->create([
+            'created_user_id' => $user->id, 
+        ]);
+
+        $file = UploadedFile::fake()->create('test.txt', 500, 'text/plain');
+        $referer = route('tasks.edit', ['project' => $project->id, 'task' => $task->id]);
+
+        $response = $this->actingAs($user)
+        ->from($referer)
+        ->post(route('task_pictures.store', ['project' => $project->id, 'task' => $task->id]),[
+            'file' => $file,
+        ]);
+
+        Storage::disk('public')->assertMissing($file->hashName());
+        $response->assertInvalid();
+        $response->assertRedirect($referer);
+    }
+
+    /**
+     * @test
+     */
+    public function 指定した容量未満の画像を登録することができる()
+    {
+        $user = User::factory()->create();
+        $project = Project::factory()->create([
+            'created_user_id' => $user->id,
+        ]);
+        $task = Task::factory()->create([
+            'created_user_id' => $user->id, 
+        ]);
+
+        $max_size_mb = 10.05;
+        $file_size_kb = $max_size_mb * 1024 - 1; //指定容量10.5MB（10,500KB未満は保存できる）
+        $file = UploadedFile::fake()->create('sizetest.jpg', $file_size_kb, 'image/jpeg');
+        $referer = route('tasks.edit', ['project' => $project->id, 'task' => $task->id]);
+
+        $response = $this->actingAs($user)
+        ->from($referer)
+        ->post(route('task_pictures.store', ['project' => $project->id, 'task' => $task->id]),[
+            'file' => $file,
+        ]);
+
+        Storage::disk('public')->assertExists($file->hashName());
+        $response->assertRedirect($referer);
+    }
+
+    /**
+     * @test
+     */
+    public function 指定した容量以上の画像を登録することはできない()
+    {
+        $user = User::factory()->create();
+        $project = Project::factory()->create([
+            'created_user_id' => $user->id,
+        ]);
+        $task = Task::factory()->create([
+            'created_user_id' => $user->id, 
+        ]);
+
+        $max_size_mb = 10.05;
+        $file_size_kb = $max_size_mb * 1024; //指定容量10.5MB（10,500KB未満は保存できる）
+        $file = UploadedFile::fake()->create('sizetest.jpg', $file_size_kb, 'image/jpeg');
+        $referer = route('tasks.edit', ['project' => $project->id, 'task' => $task->id]);
+
+        $response = $this->actingAs($user)
+        ->from($referer)
+        ->post(route('task_pictures.store', ['project' => $project->id, 'task' => $task->id]),[
+            'file' => $file,
+        ]);
+
+        Storage::disk('public')->assertMissing($file->hashName());
+        $response->assertInvalid();
+        $response->assertRedirect($referer);
+    }
+
+    /**
+     * @test
+     */
+    public function 画像は指定枚数まで保存することができる()
+    {
+        $user = User::factory()->create();
+        $project = Project::factory()->create([
+            'created_user_id' => $user->id,
+        ]);
+        $task = Task::factory()->create([
+            'created_user_id' => $user->id, 
+        ]);
+
+        $max_count = 5;
+        $file_names = [];
+        for ($count=0; $count < $max_count; $count++) { 
+            $name = 'count_test_file_'.($count+1).'.jpg';
+            $file = UploadedFile::fake()->image($name);
+            $referer = route('tasks.edit', ['project' => $project->id, 'task' => $task->id]);
+
+            $response = $this->actingAs($user)
+            ->from($referer)
+            ->post(route('task_pictures.store', ['project' => $project->id, 'task' => $task->id]),[
+                'file' => $file,
+            ]);
+            $file_names[] = $file->hashName();
+        }
+
+        for ($count=0; $count < $max_count; $count++) { 
+            Storage::disk('public')->assertExists($file_names[$count]);
+        }
+        $response->assertRedirect($referer);
+    }
+
+    /**
+     * @test
+     */
+    public function 指定枚数を超えた画像は保存できない()
+    {
+        $user = User::factory()->create();
+        $project = Project::factory()->create([
+            'created_user_id' => $user->id,
+        ]);
+        $task = Task::factory()->create([
+            'created_user_id' => $user->id, 
+        ]);
+
+        $max_count = 5;
+        $file_names = [];
+        for ($count=0; $count < $max_count+1; $count++) { 
+            $name = 'count_test_file_'.($count+1).'.jpg';
+            $file = UploadedFile::fake()->image($name);
+            $referer = route('tasks.edit', ['project' => $project->id, 'task' => $task->id]);
+
+            $response = $this->actingAs($user)
+            ->from($referer)
+            ->post(route('task_pictures.store', ['project' => $project->id, 'task' => $task->id]),[
+                'file' => $file,
+            ]);
+            $file_names[] = $file->hashName();
+        }
+        $this->assertArrayNotHasKey($max_count+1,$file_names);
+        /* @FIXME
+        assertInvalidについてはバリデーションエラーでなければ使えない可能性が浮上。
+        画像枚数のチェックはモデルの処理としておこなっているためエラーの保存場所が異なる。
+        $response->ddsession()で他のテストと比べてみると確認可能。
+        動作については確認済みなので後で時間を見て修正に挑戦してみる。
+        */
+        // $response->assertInvalid();
+        $response->assertRedirect($referer);
+    }
+}
