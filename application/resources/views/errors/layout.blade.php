6a8997efa6a1ad02358f39335a36d1c019a77fef
fjsho <gorimura7@gmail.com>
2022-03-19 12:12:38 +0900

    [fix]エラーページのcssをスタイルシートとして分離

--- application/resources/views/errors/layout.blade.php
+++ application/resources/views/errors/layout.blade.php
@@ -11,39 +11,8 @@
         <link href="https://fonts.googleapis.com/css2?family=Nunito&display=swap" rel="stylesheet">
 
         <!-- Styles -->
-        <style>
-            html, body {
-                background-color: #fff;
-                color: #636b6f;
-                font-family: 'Nunito', sans-serif;
-                font-weight: 100;
-                height: 100vh;
-                margin: 0;
-            }
+        <link rel="stylesheet" href="{{ asset('css/errors-layout.css') }}">
 
-            .full-height {
-                height: 100vh;
-            }
-
-            .flex-center {
-                align-items: center;
-                display: flex;
-                justify-content: center;
-            }
-
-            .position-ref {
-                position: relative;
-            }
-
-            .content {
-                text-align: center;
-            }
-
-            .title {
-                font-size: 36px;
-                padding: 20px;
-            }
-        </style>
     </head>
     <body>
         <div class="flex-center position-ref full-height">
